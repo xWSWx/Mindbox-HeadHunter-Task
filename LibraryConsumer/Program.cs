05e068b96fa7d9da7d6014e4574128449d87f974
xWSWx <wsw.93@yandex.ru>
2024-06-29 20:24:44 +0600

    Вывел в nuget package
    Проверил, что работает и обновляется

---
+++ LibraryConsumer/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MindboxHeadHunterTask;
+namespace LibraryConsumer
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            MindboxHeadHunterTask.Class1 class1 = new Class1();
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+
+}
